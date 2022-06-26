cbee25bee9dd3df3a89352f65988159f07dabcee
developerRangan <rangandascr7@gmail.com>
2022-06-26 13:20:50 +0530

    initialized firebase project

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss';
 
+
 function App() {
   return (
     <div>
---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+    apiKey: "AIzaSyAlmrXto0Xdlprb5tydOCzjDhqSZ2n2wis",
+    authDomain: "chat-web-app-9c97d.firebaseapp.com",
+    projectId: "chat-web-app-9c97d",
+    storageBucket: "chat-web-app-9c97d.appspot.com",
+    messagingSenderId: "555238010797",
+    appId: "1:555238010797:web:a59b212344c808988cbb24"
+  };
+
+const app = firebase.initializeApp(config);
