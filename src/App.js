5cdbaf6ebe3491fee1da410dbffcddb665ea98c3
developerRangan <rangandascr7@gmail.com>
2022-06-28 21:45:53 +0530

    manage user with profile context; put real time subs

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import SignIn from "./pages/SignIn";
 import PrivateRoute from "./components/PrivateRoute";
 import Home from "./pages/Home";
 import PublicRoute from "./components/PublicRoute";
+import { ProfileProvider } from "./context/profile.context";
   
 
 
 function App() {
   return (
+    <ProfileProvider>
     <Switch>
       <PublicRoute path="/signin">
         <SignIn/>
@@ -19,6 +21,7 @@ function App() {
         <Home/>
       </PrivateRoute>
     </Switch>
+    </ProfileProvider>
   );
 }
 
